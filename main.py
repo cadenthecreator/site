50ce3f8d9f9c810d7211fe23e9567d42ee3e5fd7
Caden Ream <114967401+cadenthecreator@users.noreply.github.com>
2024-05-07 21:49:28 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import os
+hostName = "cadenthecreator.github.io"
+serverPort = 80
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print(self.path)
+        try:
+            file = open("./files"+self.path, "rb")
+            self.send_response(200)
+            self.wfile.write(file.read())
+            file.close()
+        except:
+            print("uh oh")
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
